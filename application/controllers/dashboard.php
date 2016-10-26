320db923c2fb500ac40cb20ffce4e22ee90c6dd6
JirachotGG <jirachot23@gmail.com>
2016-10-26 19:21:57 +0700

    Edit sitebar

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -14,21 +14,24 @@ class dashboard extends CI_Controller {
 
   public function maindashboard()
   {
-
+    $this->load->model("menu_model", "menu");
+    $data['result'] = $this->menu->getall();
     $this->load->view('layout_dashboard/header');
     $this->load->view('layout_dashboard/navbar');
-    $this->load->view('layout_dashboard/sitebar');
+    $this->load->view('layout_dashboard/sitebar', $data);
     $this->load->view('dashboard/dashboard');
     $this->load->view('layout_dashboard/footer');
   }
 
   public function setting()
   {
+    $this->load->model("menu_model", "menu");
+    $menu['result'] = $this->menu->getall();
     $this->load->model("setting_model", "setting");
     $data['result'] = $this->setting->getall();
     $this->load->view('layout_dashboard/header');
     $this->load->view('layout_dashboard/navbar');
-    $this->load->view('layout_dashboard/sitebar');
+    $this->load->view('layout_dashboard/sitebar', $menu);
     $this->load->view('dashboard/setting', $data);
     $this->load->view('layout_dashboard/footer');
 
@@ -36,11 +39,13 @@ class dashboard extends CI_Controller {
 
   public function slider()
   {
+    $this->load->model("menu_model", "menu");
+    $menu['result'] = $this->menu->getall();
     $this->load->model("slide_model", "slide");
     $data['result'] = $this->slide->getall();
     $this->load->view('layout_dashboard/header');
     $this->load->view('layout_dashboard/navbar');
-    $this->load->view('layout_dashboard/sitebar');
+    $this->load->view('layout_dashboard/sitebar', $menu);
     //$this->load->vars($data);
     $this->load->view('dashboard/slider', $data);
     $this->load->view('layout_dashboard/footer');
