87af5d70569c7c11bc72e14eec101372f47b4372
iMaew <imaew@iMaew.local>
2016-11-22 22:28:57 +0700

    View Login

---
+++ application/views/home/login.php
@@ -0,0 +1,54 @@
+<div class="content_container" style="background:#ffffff url(images/content_bg2.png) repeat-x left bottom;">
+	<div class="bidnow_row">
+   		<div class="content_middlebg" style="padding-top:0px;">
+      		<div class="myaccount_container">
+    	        <div class="login_main_container" style="width:980px;">
+            <div class="allauctions_heading_row" style="width:980px;">
+                <div class="myaccount_heading_leftbg">&nbsp;</div>
+                    <div class="static_pg_heading_middlebg" style="width:967px;">BidCups เข้าสู่ระบบ</div>
+                <div class="myaccount_heading_rightbg">&nbsp;</div>
+            </div>
+            <div class="login_content_inner" style="width:977px;">
+            	<div style="width:930px; float:left; padding-left:1px;">
+                    <div class="login_box">
+                    <form name="f1" method="post" action="password.php" >
+                        <div class="login_bottom_login_box_inner">
+
+                                                        <div class="login_bottom_login_title">ได้ลงทะเบียนหรือยังครับ</div>
+                            <div class="login_bottom_login_inner_content">กรอกข้อมูลเพื่อเริ่มสนุกกับ BidCups ตรงนี้เลยครับ</div>
+                                                        <div class="login_bottom_login_inner_box">
+                                <div>Username&nbsp;/&nbsp;อีเมล์:</div>
+                                <div class="login_username_box_content"><input type="text" name="username" id="username1" class="logintextboxclas" autofocus /></div>
+                                <div class="login_password_box_title">รหัสผ่าน:</div>
+                                <div class="login_password_box_content"><input type="password" name="password" id="password1" class="logintextboxclas" maxlength="16" /></div>
+                                <div class="login_bottom_login_button">
+                                    <button type="submit" class="btn btn-bid btn-sml">เข้าสู่ระบบ</button>
+                                </div>
+                                <!--<div class="login_bottom_forgot_password"><a href='forgotpassword'>ลืม username หรือ รหัสผ่าน?</a></div>!-->
+                                </div>
+                        </div>
+                        <input type="hidden" value="" name="wonauctions" />
+                    </form>
+                    </div>
+                	<div id="register_box" style="background: url(https://www.dinoza.com/images/thai/login_voucher.gif) no-repeat;">
+						<div class="login_bottom_register_box_inner">
+							<div class="login_bottom_register_title"><strong>เริ่มเล่นได้เลย</strong></div>
+							<div class="login_bottom_register_content">BidCups -- เว็บไซต์การประมูลที่น่าตื่นเต้นที่สุดบนอินเตอร์เน็ต</div>
+							<div class="login_bottom_register_content_ul">
+								<ul type="disc">
+									<li>ประมูลชนะผลิตภัณฑ์ใหม่ๆ จากรายการมันส์ๆ ทุกวัน!</li>
+									<li>รับฟรีส่วนลด 100 บาท สมัครสมาชิก BidCups!</li>
+									<li>ลงทะเบียนฟรี และรับ Bid เพิ่มทันทีเมื่อแนะนำเแล้วพื่อนซือ!</li>
+								</ul>
+								<div class="login_bottom_register_button"><a href="<?php echo site_url('home/regis');?>" class="btn btn-bid btn-sml">ลงทะเบียน</a></div>
+							</div>
+						</div>
+					</div>
+					<div style="height: 15px;" class="clear">&nbsp;</div>
+                </div>
+            </div>
+            </div>
+    	</div>
+     	</div>
+     </div>
+</div>
