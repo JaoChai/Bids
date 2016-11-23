1087f0c4bcf98548fad68f5145d8d0ca470798ab
iMaew <imaew@iMaew.local>
2016-11-23 18:39:41 +0700

    View Product

---
+++ application/views/home/productdetail.php
@@ -0,0 +1,279 @@
+<div class="content_container">
+	<div class="bidnow_row">
+       <div class="content_middlebg">
+            <div class="myaccount_container">
+            <div class="allauctions_container" style="width:980px;">
+                <div class="allauctions_heading_row">
+                      <div class="allauctions_heading_row" style="width:980px;">
+                         <div class="myaccount_heading_leftbg">&nbsp;</div>
+                            <div class="static_pg_heading_middlebg">
+                              <h1 class="heading_product_name">
+                              เครื่องดูดฝุ่นในรถแบบพกพา ถึงฝุ่นหนาเต็มรถจนน่ากลัว ดูดรัวรัวฝุ่นหายชัวร์เบาะนวลตา
+                              </h1>
+                        </div>
+                        <div class="myaccount_heading_rightbg">&nbsp;</div>
+                      </div>
+
+				<div class="product_details_content_bg" style="width:977px; margin-bottom:10px;">
+                        <div class="product_details_content_inner" style="width:977px;">
+                            <div class="product_details_inner_image" style="padding-left:13px;">
+                                <div class="product_details_inner_image_detailbox" style="width:368px;">
+
+                                    <div class="product_details_inner_image_body" id="detailbodymargin">
+                                        <div class="product_details_inner_image_big">
+                                          <span id="mainimage1"><img src="https://www.dinoza.com/uploads/products/popup/popup_f32b80870bcfbf074984010a2d5f110a.jpg" alt="" /></span>
+
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div class="product_details_inner_auction product_details_inner_auction_4496"  id="" >
+                                <div class="auction-item" style="display: none" title="4496" id=""></div>
+                                    <div class="product_details_inner_auction_detailbox">
+                                        <div class="product_details_inner_auction_body" id="detailbodymargin">
+                                            <div class="product-detail-market-price-row">ลดจากราคา&nbsp;<span class="marketprice_det">฿35100</span></div>
+
+                                            <div class="product-price-row">
+                                            	<span class="product_price_row_top">ประมูล ประหยัดสูงสุด</span>
+                                                <span class="product_price_row_bottom">99%</span>
+                                            </div>
+
+
+                                            <div class="detail-row" style="padding-top:10px;">
+                                                <div class="textnew right1" style="font-size:24px; font-weight:bold;">
+                                                <div class="detailtopprice-container">
+                                                    <div class="detailtopprice_pd" style="padding-bottom:7px; height:25px; padding-top:0px; display:inline-block; #margin-top:-3px; color:#059E0E;" id="currencysymbol_4496">
+                                                    </div><div class="detailtopprice_pd" id="price_index_page_4496" style=" padding-bottom:5px; height:27px; color:#059E0E; padding-top:0px; display:inline-block;">00:00:00</div>
+                                                </div>
+                                            </div>
+                                            </div>
+
+                                            <div class="detail-row">
+                                                <div class="right1" style="width:313px; text-align:center; font-weight:bold; height:25px;"><span class="prod_biddertext" id="product_bidder_4496" style="color:#5B5B5B; width:313px; text-align:center; font-size:18px;">120</span>&nbsp;&nbsp;</div>
+                                            </div>
+                                            <div class="detail-row" style="margin-bottom:0px;">
+                                                <div class="toptimer_prod" style="color:#666666">
+                                                    <span id="counter_index_page_4496">
+
+                                                    <script language=javascript>
+                                                          document.getElementById('counter_index_page_4496').innerHTML = calc_counter_from_time('11871');
+                                                    </script>
+
+                                                  </span>
+                                                </div>
+                                            </div>
+
+                                            <div class="detail-row">
+                                                <div class="normal_text_small_new">
+                                                	<span id="normal_text_bid_info_enable_4496" style="display:inline-block;top: -4px;">
+                                                          ทุก Bid เพิ่ม 10วิ
+                                                  </span>
+                                                </div>
+                                            </div>
+
+                                            <div class="detail-row">
+
+                                            <span class="btn btn-org btn-bid" alt="Bid" onclick="javascript: window.location.href='login.php?src=pdbidbtn'" id="image_main_4496">Bid</span>
+
+
+                                            </div>
+
+
+
+                                        </div>
+                                    </div>
+                                    <div class="prod-detailbox2">
+                                      <div class="prod-detail-body2 zeropc" id="detailbodymargin" style="height:190px;padding:5px 0;">
+
+                                            <div class="product_detail_amount_info_heading_main"></div>
+
+
+                                            </div>
+										                      </div>
+                                      </div>
+
+
+
+                            <div class="prod-detailbox-container">
+                            <!--<div class="prod-detailbox2" style="height:64px; margin-top:0px; padding-top:17px;">
+                              <div id="auctiontypeicon" class="auction_image1">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     <a onmouseover="javascript:showhide_auctype(6,'over');" onmouseout="javascript:showhide_auctype(6,'out');" style="cursor:pointer"><img src="images/thai/buynow_big.png" title="สามารถซื้อสินค้าได้" alt="" /></a>&nbsp;
+                                                                                                                                                                                                                                                                                                                                                          </div>
+                                                                <div id="fbbtn">
+                                    <fb:like href="https://www.dinoza.com/productdetails.php?aid=4496" layout="box_count" width="175" send="false" ></fb:like>
+                                </div>
+                                                            </div>-->
+                            <div class="clear"></div>
+                            <!-- <div id="notification_help" style="display:none;">
+                                <div class="email_notification-detail-body" style="margin-top:-95px;">
+                                    <div class="email_notification-detail-heading">Email notification for Help</div>
+                                    <div class="email_notification-detail-data">Email notification for Help. Please report to Veera@dinoza.com immediately. </div>
+                                </div>
+                            </div> -->
+
+                              	<div class="product_details_register-now">
+                                    <a href="<?php echo site_url('home/regis');?>" id="signupintro"></a>
+                                </div>
+
+													<!--Bid History Box Start Here-->
+                            <div class="product_detail_bidhistory_main_box">
+                                <div id="producthistory1">
+                                    <div class="product_detail_bishistory_top">
+                                      <div class="product-history-full" style="border-bottom:0px solid;"><div class="autobidder_title" >ประวัติการประมูล</div></div>
+                                      </div>
+                                        <div class="product_detail_bidhistory_body">
+                                            <div class="divclear"></div>
+                                            <div class="product_detail_bidhistory_title_row">
+                                                <div class="product_detail_bidhistory_title_bid">ราคา</div>
+                                                <div class="product_detail_bidhistory_title_bidder">ผู้ประมูล</div>
+                                                <div class="product_detail_bidhistory_title_bidtype">ประเภท</div>
+                                            </div>
+                                              <div class="product_detail_bidhistory_inner_main">
+                                                    <div class="product_detail_bidhistory_inner_bid" id="bid_price_0">
+                                                          ฿1.30
+                                                    </div>
+                                                    <div class="product_detail_bidhistory_inner_bidder" id="bid_user_name_0">
+                                                          kakaashi
+                                                    </div>
+                                                    <div class="product_detail_bidhistory_inner_bidtype" id="bid_type_0">
+                                                          Manual Bid
+                                                    </div>
+                                                </div>
+
+                                                </div>
+                                        </div>
+                                 </div>
+                                 <div id="producthistory1_hidden" style="display: none">
+                                    <div class="product_detail_bishistory_top">
+                                        <div class="product-history-leftside1" onclick="hideDisplayBids(1);"><div class="autobidder_title_new" style="font-size: 10px;">ประวัติการประมูล</div></div>
+                                        <div class="product-history-rightside1" onclick="hideDisplayBids(2);"><div class="autobidder_title_new" style="font-size: 12px; font-weight:bold;">Bid ที่ลงไป</div></div>
+                                    </div>
+                                    <div class="product_detail_bidhistory_body">
+                                        <div class="product_detail_bidhistory_title_row">
+                                            <div class="product_detail_bidhistory_title_bid">ราคา</div>
+                                            <div class="product_detail_bidhistory_title_bidtime">เวลา</div>
+                                            <div class="product_detail_bidhistory_title_bidtype_2">ประเภท</div>
+                                        </div>
+                                                                            </div>
+                                 </div>
+
+                            </div>
+                            <!--Bid History Box End Here-->
+                            </div>
+                        </div>
+                    </div>
+             </div>
+
+			 			 <!--Product Detail Testimonial Start-->
+             <div class="alltestimonial_container_main">
+             	<div class="alltesetimonial_container_image">
+                	<div class="testimonialvideo">
+                	        <img src="https://www.dinoza.com/uploads/testimonial/1463071100_271921.jpg" width="196" height="156" />
+                            <div class="alltestimonial_username">weradet</div>
+                    </div>
+                </div>
+                <div class="alltestimonial_thatsbigbargain">ซื้อได้ถูกจริง อะไรจริง!</div>
+                <!-- <div class="recent_ended_auction_since_launch"><a href="winners" class="recent_ended_auction_since_launch">ดูอีก&nbsp;<strong>3799</strong>&nbsp;คนที่ได้ราคาดีขนาดนี้</a></div> -->
+                <div class="alltestimonial_detail_description">เราการันตีความถูกที่ BidCups.com</div>
+                <div class="alltestimonial_detail_description">มีหลายคนแล้วที่ประสบความสำเร็จกับ BidCups ได้ของไปในราคาที่เหลือเชื่อ</div>
+                <div class="alltestimonial_detail_nextcouldbeyou">คนต่อไปอาจเป็นคุณ&nbsp;<span><a href="registration.php">สมัครเลย</a></span></div>
+
+             </div>
+
+
+
+
+			 <!--Product Detail Box Start Here -->
+
+				<div class="allauctions_container" style="width:980px;">
+                  <div class="allauctions_heading_row" style="width:980px;">
+                    <div class="myaccount_heading_leftbg">&nbsp;</div>
+                        <div class="static_pg_heading_middlebg">รายละเอียดสินค้า</div>
+						<!-- <span class="history_auctionid" id="history_auctionid">4496</span><span class="item_no" id="item_no" style="position:absolute;top:0;right:55px;float:right;font-size: 20px;background:#eee;padding: 4px 0 4px 8px;">Item #</span> -->
+                    <div class="myaccount_heading_rightbg">&nbsp;</div>
+                </div>
+                <div class="allauctions_contentbg">
+                    <div class="product_detail_body_middle">
+                    <div class="product_details_desc_box">
+                      <!-- Loop Content This Here !-->
+
+                      <p>เครื่องดูดฝุ่นภายในรถยนต์แบบพกพา
+                      ขจัดปัดเป่าสิ่งปฏิกูล ที่เพิ่มพูนภายในรถ! พริบตาเดียวสะอาดเหมือนได้รถใหม่ๆออกจากโชว์รูม ใช้งานง่ายๆเพียงเสียบตัวต่อไฟเข้ากับที่จุดบุหรี่ในรถของคุณก็ใช้งานได้ง่ายดาย แถมที่ดูดฝุ่นแสนยาว ที่สามารถเก็บรายละเอียดสิ่งสกปรกตามซอกมุมให้เรียบหมดจด!</p>
+
+                      <p>คุณสมบัติ
+                      ดูดฝุ่น“ลด”คราบ ให้ “รถ” คุณสะอาดเหมือนใหม่ ด้วยเครื่องดูดฝุ่นในรถยนต์พลังแรง
+                      เครื่องดูดฝุ่นในรถยนต์ Car Vacuum Cleaner มีรายละเอียดดังนี้
+                      ผลิตจากพลาสติก ABS คุณภาพดี แข็งแรงทนทาน
+                      ใช้งานกับไฟรถยนต์เพียงต่อเข้ากับที่จุดบุหรี่ในรถ
+                      ให้พลังการดูดที่ทรงพลัง สามารถดูดได้ทั้งขยะแห้งและขยะชื้น มาพร้อมหัวต่อขนาดยาว สำหรับใช้ดูดตามซอกมุมที่ยากต่อการเข้าถึง
+                      มีสวิตซ์เปิด
+                      ปิดควบคุมการทำงาน
+                      กะทัดรัดน้ำหนักเบา พกพาสะดวก ประหยัดพื้นที่ในการจัดเก็บ
+                      ขนาดโดยประมาณ 25.5 x 9 x 11 ซม.
+                      น้ำหนัก 1.5 กก.
+                  </div>
+                </div>
+
+                <div class="divclear"></div>
+              </div>
+            </div>
+            <!-- End Content This Here !-->
+        </div>
+      </div>
+            <!--Product Detail Box End Here-->
+            <!--Shipping Detail Box Start Here-->
+            <div class="allauctions_container">
+                <div class="allauctions_heading_row" style="width:980px;">
+                    <div class="myaccount_heading_leftbg">&nbsp;</div>
+                       <div class="static_pg_heading_middlebg" style="width:969px;">ค่าจัดส่ง</div>
+                     <div class="myaccount_heading_rightbg">&nbsp;</div>
+                </div>
+                <div class="allauctions_contentbg" style="width:935px;">
+                    <div class="shippingtitle" style="width:875px;">
+                                <div>จัดส่งภายใน 7 - 10 วันทำการ&nbsp;฿50.00</div>
+                      <div class="clear">&nbsp;</div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <!--Shipping Detail Box end Here-->
+            <!--ShareThis Box Start Here-->
+            <div class="allauctions_container">
+                <div class="allauctions_heading_row" style="width:980px;">
+                    <div class="myaccount_heading_leftbg">&nbsp;</div>
+                       <div class="static_pg_heading_middlebg" style="width:969px;">Share the excitement!</div>
+                     <div class="myaccount_heading_rightbg">&nbsp;</div>
+                </div>
+
+                <div class="allauctions_contentbg" style="width:935px;">
+                    <div id="sharethis">
+                    <!--ShareThis Box Start Here-->
+                            <ul class="social-share">
+                                <li class="facebook"><a target="_blank" href="#" title="Share on Facebook!"><i class="fa fa-fw fa-facebook-square"></i></a></li>
+                                <li class="twitter"><a target="_blank" href="#" title="Share on Twitter!"><i class="fa fa-fw fa-twitter-square"></i></a></li>
+                                <li class="google-plus"><a target="_blank" href="#" title="Share on Google Plus!"><i class="fa fa-fw fa-google-plus-square"></i></a></li>
+                                <!-- <li class="pinterest"><a target="_blank" href="#" class="pin-it-button" count-layout="horizontal" title="Share on Pinterest"><i class="fa fa-fw fa-pinterest-square"></i></a></li> -->
+                                <!-- <li class="email-share"><a title="Share by email" href="mailto:?subject=Apple iPhone 6s (128GB) สิ่งเดียวที่เปลี่ยนไปคือ ทุกสิ่ง ราคาเริ่มต้น 1 บาท! ที่ Dinoza.com&body=Apple iPhone 6s (128GB) สิ่งเดียวที่เปลี่ยนไปคือ ทุกสิ่ง ราคาเริ่มต้น 1 บาท! ที่ Dinoza.com https://www.dinoza.com/productdetails.php?aid=4496&title=Apple iPhone 6s (128GB) สิ่งเดียวที่เปลี่ยนไปคือ ทุกสิ่ง ราคาเริ่มต้น 1 บาท! ที่ Dinoza.com"><i class="fa fa-fw fa-envelope"></i></a></li> -->
+                            </ul>
+                        <div class="clear"></div>
+                    <!--ShareThis Box End Here-->
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <!--ShareThis Box End Here-->
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="banner-sub" class="banner_main_box" style="margin-top: 15px; margin-bottom: 15px; min-height: 0;">
+			    	<div class="banner" style="min-height: 0;">
+					      <a href='dpoints?ref=homebnr'><img src="http://localhost:8888/auction/assets/home/images/5-D-points-1040x218-3.png" style="width: 100%;" /></a>
+				    </div>
+				</div>
+
+			  <div class="clearfix"></div>
